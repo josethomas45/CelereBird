d0ad241f7507156a50dc43e9895cd8f970db4ad1
Jose thomas <josethomas2026@cs.sjcetpalai.ac.in>
2025-03-18 11:25:22 +0530

    making mail changes

--- src/app/mail/mail.tsx
+++ src/app/mail/mail.tsx
@@ -1,1 +1,19 @@
+import React from 'react' 
+import { ResizableHandle , ResizablePanel , ResizablePanelGroup } from '@/components/ui/resizable' 
+import { TooltipProvider } from '@radix-ui/react-tooltip'
  
+ const Mail = () => {
+    return (
+        <TooltipProvider delayDuration={0}>
+            <ResizablePanelGroup direction='horizontal' onLayout={(sizes: number[]) =>{
+                console.log(sizes)
+            }} className='items-stretch h-full min-h-screen'>
+                <ResizablePanel minSize={20} maxSize={40} className='bg-red-500'></ResizablePanel>
+
+        
+            </ResizablePanelGroup>
+        </TooltipProvider>
+    )
+ }
+
+ export default Mail
